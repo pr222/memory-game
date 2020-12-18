cb8f27f8063400699a5b12273a598af2a9f7a3cf
Pauliina Raitaniemi <pr222ja@student.lnu.se>
2020-12-18 11:45:57 +0100

    Add a reset button

--- src/components/a-memory-game/a-memory-game.js
+++ src/components/a-memory-game/a-memory-game.js
@@ -6,6 +6,7 @@
  */
 
 const backImg = (new URL('img/0.png', import.meta.url)).href
+let nrOfCards = 0
 
 /**
  * Define the template
@@ -13,10 +14,15 @@ const backImg = (new URL('img/0.png', import.meta.url)).href
 const template = document.createElement('template')
 template.innerHTML = `
   <style> 
+  :host {
+      font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+      color: #222222;
+  }
 
   #buttonMenu {
       display: block;
       padding: 40px;
+      text-align: center;
       background-color: cadetblue;
   }
 
@@ -30,7 +36,7 @@ template.innerHTML = `
       display: inline-block;   
   }
 
-  li button {
+  button {
       display: block;
       padding: 10px;
       padding-left: 20px;
@@ -44,7 +50,7 @@ template.innerHTML = `
   button:focus, button:hover {
       outline: none;
       box-shadow: 0px 10px 30px;
-      border: 3px solid #8c2323; 
+      border: 3px solid #222222; 
   }
 
   .hidden {
@@ -53,17 +59,21 @@ template.innerHTML = `
   </style>
 
   <!-- TODO: Add html here  -->
-  <div id="wrapper">
+  <div id="mainWrapper">
     <div id="buttonMenu">
+        <h1>Choose your game:</h1>
         <ul>
             <li><button type="button" id="button2x2">2x2</button></li>
             <li><button type="button" id="button4x2">4x2</button></li>
             <li><button type="button" id="button4x4">4x4</button></li>
         </ul>
     </div>
-    <div id="grid2x2"></div>
-    <div id="grid4x2"></div>
-    <div id="grid4x4"></div>
+    <div id="gridWrapper">
+        <div id="grid2x2"></div>
+        <div id="grid4x2"></div>
+        <div id="grid4x4"></div>
+    </div>
+    <div><button type="button" id="resetButton">Reset</button></div>
   </div>
 `
 /**
@@ -84,10 +94,12 @@ customElements.define('a-memory-game',
       this.attachShadow({ mode: 'open' })
         .appendChild(template.content.cloneNode(true))
 
-      this._wrapper = this.shadowRoot.querySelector('#wrapper')
-      // this._smallGrid = this.shadowRoot.querySelector('#grid2x2')
-      // this._mediumGrid = this.shadowRoot.querySelector('#grid4x2')
-      // this._bigGrid = this.shadowRoot.querySelector('#grid4x4')     
+      this._wrapper = this.shadowRoot.querySelector('#mainWrapper')
+      this._smallGrid = this.shadowRoot.querySelector('#grid2x2')
+      this._mediumGrid = this.shadowRoot.querySelector('#grid4x2')
+      this._bigGrid = this.shadowRoot.querySelector('#grid4x4')
+
+      this._onClick = this._onClick.bind(this)
     }
 
     /**
@@ -113,10 +125,48 @@ customElements.define('a-memory-game',
         console.log('Click event!')
         if (event.target.id === 'button2x2') {
             console.log('Display a 2x2 grid.')
+            nrOfCards = 4
+            this._renderCards('2x2')
         } else if (event.target.id === 'button4x2') {
             console.log('Display a 4x2 grid.')
+            nrOfCards = 8
+            this._renderCards('4x2')
         } else if (event.target.id === 'button4x4') {
             console.log('Display a 4x4 grid.')
+            nrOfCards = 16
+            this._renderCards('4x4')
+        }
+
+        if (event.target.id === 'resetButton') {
+            console.log('Reset the game!')
+            this._resetGame()
+        }
+    }
+
+    _resetGame () {
+        console.log('Resetting the game...')
+        nrOfCards = 0
+    }
+
+    /**
+     * Render out cards for current game.
+     *
+     * @param {string} grid - What type of grid should be displayed.
+     */
+    _renderCards(grid) {
+        switch(grid) {
+            case '2x2':
+                console.log('Render a 2x2 grid.')
+                this._smallGrid.textContent = '2x2'
+                break
+            case '4x2':
+                console.log('Render a 4x2 grid.')
+                this._mediumGrid.textContent = '4x2'
+                break
+            case '4x4':
+                console.log('Render a 4x4 grid.')
+                this._bigGrid.textContent = '4x4'
+                break
         }
 
     }
